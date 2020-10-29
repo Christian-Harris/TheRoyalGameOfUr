c53922a9bbb039c4847d9a0525195bfd84305799
Christian Harris <harris.chrw@gmail.com>
2020-10-29 12:50:13 -0600

    Removed files no longer being used.

--- src/game/GameMove.java
+++ src/game/GameMove.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 public class GameMove implements Serializable{
 	private int type;
 	private Object data;
+	private PlayerCode playerCode;
 	
-	public GameMove(int type, Object data){
+	public GameMove(int type, Object data, PlayerCode playerCode){
 		this.type = type;
 		this.data = data;
+		this.playerCode = playerCode;
 	}
 	
 	public int getType(){
@@ -17,5 +19,9 @@ public class GameMove implements Serializable{
 	
 	public Object getData(){
 		return this.data;
+	}
+	
+	public PlayerCode getPlayerCode(){
+		return this.playerCode;
 	}
 }
